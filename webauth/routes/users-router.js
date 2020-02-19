6641ac726478b43601cb1dfae5db400cf7518fb0
Denise Chaloupka <denisechaloupka@yahoo.com>
2020-02-18 21:17:15 -0600

    MVP

---
+++ webauth/routes/users-router.js
@@ -0,0 +1,15 @@
+const usersRouter = require('express').Router();
+const restricted = require('../middleware/restricted-middleware')
+const Users = require('../data/models/user-model');
+
+usersRouter.get('/', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
+
+module.exports = usersRouter;
---
+++ webauth/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const logger = require('morgan');
+require('dotenv').config();
+
+const registerRouter = require('../routes/register-router')
+const loginRouter = require('../routes/login-router')
+const usersRouter = require('../routes/users-router')
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(logger('dev'));
+
+server.use('/api/register', registerRouter)
+server.use('/api/login', loginRouter)
+server.use('/api/users', usersRouter)
+
+module.exports = server;
